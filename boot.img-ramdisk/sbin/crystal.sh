ad8860eeb31b356508186bd9340d74e5c4393017
mastero9017 <mastero9017@gmail.com>
2015-04-06 22:21:34 +0200

    Ramdisk update

--- boot.img-ramdisk/sbin/crystal.sh
+++ boot.img-ramdisk/sbin/crystal.sh
@@ -1,111 +1,57 @@
 #!/system/bin/sh
+# sleep 25
 
-CFG="/sdcard/chroma.cfg"
+sed -i 's/dalvik.vm.image-dex2oat-filter.*/dalvik.vm.image-dex2oat-filter=everything/g' /system/build.prop
+sed -i 's/dalvik.vm.dex2oat-filter=.*/dalvik.vm.dex2oat-filter=everything/g' /system/build.prop
 
-if [ ! -f $CFG ]; then
-	cp /system/addon.d/00-chroma_kernel.cfg $CFG;
-	chmod 755 $CFG;
-fi
+# Color Control
+#  Red Positive                = /sys/module/dsi_panel/kgamma_rp
+echo "0 12 19 30 39 48 56 72 82 104 118 127 119 116 115 106 84 78 66 60 44 35 20" > /sys/module/dsi_panel/kgamma_rp
+#  Red Negative                = /sys/module/dsi_panel/kgamma_rn
+echo "0 12 19 30 39 48 56 72 82 104 118 131 120 116 114 107 100 78 66 60 44 35 20" > /sys/module/dsi_panel/kgamma_rn
+#  Green Positive              = /sys/module/dsi_panel/kgamma_gp
+echo "0 12 20 31 40 55 62 76 89 109 123 132 115 113 111 103 78 75 67 58 49 39 21" > /sys/module/dsi_panel/kgamma_gp
+#  Green Negative              = /sys/module/dsi_panel/kgamma_gn
+echo "0 12 20 31 40 55 62 79 84 109 123 134 116 112 112 104 101 76 67 58 49 39 21" > /sys/module/dsi_panel/kgamma_gn
+#  Blue Positive               = /sys/module/dsi_panel/kgamma_bp
+echo "0 12 19 30 39 48 56 72 83 105 119 130 119 115 116 106 88 80 71 62 52 42 25" > /sys/module/dsi_panel/kgamma_bp
+#  Blue Negative               = /sys/module/dsi_panel/kgamma_bn
+echo "0 12 19 30 39 48 56 72 83 105 121 130 118 115 114 108 100 80 66 60 48 38 22" > /sys/module/dsi_panel/kgamma_bn
+#  White Point                 = /sys/module/dsi_panel/kgamma_w
+echo "32" > /sys/module/dsi_panel/kgamma_w
 
-###
-#
-# Start Apllying Value....
-#
-#
-# WARNING: Do not mess up here!
-#
-##
+# FSYNC I/O
+echo "N" > /sys/module/sync/parameters/fsync_enabled
 
-# Set Sweep2Wake
-s2w="`grep "s2w_cfg" $CFG | cut -d '=' -f2`"
-echo $s2w > /sys/android_touch/sweep2wake;
+# LCD
+echo 1 > /sys/module/lm3630_bl/parameters/backlight_dimmer
+echo 6 > /sys/module/lm3630_bl/parameters/min_brightness
 
-# Set Sweep2Wake
-s2s="`grep "s2s_cfg" $CFG | cut -d '=' -f2`"
-echo $s2s > /sys/android_touch/sweep2sleep;
+# Disable Logging
+echo 0 > /sys/block/mmcblk0/queue/iostats
+echo 0 > /sys/module/logger/parameters/enabled
+echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_cmd
+echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_ctxt
+echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_drv
+echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_mem
+echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_pwr
 
-# Set Doubletap2wake
-dt2w="`grep "dt2w_cfg" $CFG | cut -d '=' -f2`"
-echo $dt2w > /sys/android_touch/doubletap2wake;
+# Multicore battery saving
+echo 1 > /sys/devices/system/cpu/sched_mc_power_savings
 
-# Set S2W/DT2W Power Key Toggle
-pwrkey="`grep "pwrkey_cfg" $CFG | cut -d '=' -f2`"
-echo $pwrkey > /sys/module/qpnp_power_on/parameters/pwrkey_suspend;
+# Hardware
+echo 72 > /sys/module/msm_thermal/parameters/temp_threshold
+echo 60 > /sys/class/timed_output/vibrator/amp
+echo 0 > /sys/kernel/fast_charge/force_fast_charge
+echo lp > /proc/sys/net/ipv4/tcp_congestion_control
 
-# Set Cooler Colors
-color="`grep "color_cfg" $CFG | cut -d '=' -f2`"
-echo $color > /sys/module/mdss_dsi/parameters/color_preset;
+# Memory
+echo 0 > /proc/sys/vm/swappiness
+echo 1 > /proc/sys/vm/laptop_mode
+echo 0 > /sys/kernel/mm/ksm/run
 
-# Set Wake Vibration Strength
-vib="`grep "vib_cfg" $CFG | cut -d '=' -f2`"
-echo $vib > /sys/android_touch/vib_strength;
+# fstrim
+fstrim -v /data
+fstrim -v /cache
+fstrim -v /system
 
-# Set Wake Timeout
-wt="`grep "wt_cfg" $CFG | cut -d '=' -f2`"
-echo $wt > /sys/android_touch/wake_timeout;
-
-# Set CPU Settings (Use app to set in boot for CPU Settings only! Do not edit manually unless you know what you doing!)
-echo 1 > /sys/devices/system/cpu/cpu0/online;
-echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
-echo 1 > /sys/devices/system/cpu/cpu1/online;
-echo conservative > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
-echo 1 > /sys/devices/system/cpu/cpu2/online;
-echo conservative > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
-echo 1 > /sys/devices/system/cpu/cpu3/online;
-echo conservative > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
-echo 1190400 > /sys/devices/system/cpu/cpufreq/conservative/input_boost_freq;
-echo 40 > /sys/devices/system/cpu/cpufreq/conservative/down_threshold;
-echo 10000 > /sys/devices/system/cpu/cpufreq/conservative/sampling_rate;
-echo 40000 > /sys/devices/system/cpu/cpufreq/conservative/input_boost_duration;
-echo 90 > /sys/devices/system/cpu/cpufreq/conservative/up_threshold;
-echo 10000 > /sys/devices/system/cpu/cpufreq/conservative/sampling_rate_min;
-echo 10 > /sys/devices/system/cpu/cpufreq/conservative/freq_step;
-echo 1 > /sys/devices/system/cpu/cpufreq/conservative/sampling_down_factor;
-echo 0 > /sys/devices/system/cpu/cpufreq/conservative/ignore_nice_load;
-
-# Set CPU Max Frequency
-cpu_max="`grep "cpu_max_cfg" $CFG | cut -d '=' -f2`"
-echo $cpu_max > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-echo $cpu_max > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq;
-echo $cpu_max > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq;
-echo $cpu_max > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq;
-
-# Set CPU Min Frequency
-cpu_min="`grep "cpu_min_cfg" $CFG | cut -d '=' -f2`"
-echo $cpu_min > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
-echo $cpu_min > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq;
-echo $cpu_min > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq;
-echo $cpu_min > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq;
-
-
-# Set Max Screen Off
-max_off_enable="`grep "max_off_enable_cfg" $CFG | cut -d '=' -f2`"
-echo $max_off_enable > /sys/devices/system/cpu/cpu0/cpufreq/screen_off_max;
-
-# Set backlight dimmer
-backlight="`grep "backlight_cfg" $CFG | cut -d '=' -f2`"
-echo $backlight > /sys/module/lm3630_bl/parameters/backlight_dimmer;
-
-# Set fsync
-fsync="`grep "fsync_cfg" $CFG | cut -d '=' -f2`"
-echo $fsync > /sys/module/sync/parameters/fsync_enabled;
-
-# Set GPU Governor
-gpu_gov="`grep "gpu_gov_cfg" $CFG | cut -d '=' -f2`"
-echo $gpu_gov > /sys/class/kgsl/kgsl-3d0/pwrscale/trustzone/governor;
-
-# Set GPU Governor
-gpu_freq="`grep "gpu_freq_cfg" $CFG | cut -d '=' -f2`"
-echo $gpu_freq > /sys/class/kgsl/kgsl-3d0/max_gpuclk;
-
-# Set Global Vibration
-global_vib="`grep "global_vib_cfg" $CFG | cut -d '=' -f2`"
-echo $global_vib > /sys/class/timed_output/vibrator/amp;
-
-# Set IO scheduler
-iosched="`grep "iosched_cfg" $CFG | cut -d '=' -f2`"
-echo $iosched > /sys/block/mmcblk0/queue/scheduler;
-
-# Set Readahead
-readahead="`grep "readahead_cfg" $CFG | cut -d '=' -f2`"
-echo $readahead > /sys/block/mmcblk0/queue/read_ahead_kb;
